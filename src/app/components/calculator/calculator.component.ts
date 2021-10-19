36f36d9c4e9b10b46023d5d04cd7d3199ae6d4c8
Daniel Rojo <danielrojo@gmail.com>
2021-10-19 10:32:38 +0200

    calculator component done

--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,12 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+
+enum State {
+  Init,
+  FirstFigure,
+  SecondFigure,
+  Result
+}
 
 @Component({
   selector: 'app-calculator',
@@ -9,13 +17,106 @@ export class CalculatorComponent implements OnInit {
 
   display = '';
 
+  currentState = State.Init;
+  firstFigure = 0;
+  secondFigure = 0;
+  result = 0;
+  operator = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  handleClick(value: any) {    
-    this.display += value.toString();
+  handleNumber(value: number): void {
+    switch (this.currentState) {
+      case State.Init:
+        this.firstFigure = value;
+        this.currentState = State.FirstFigure;
+        this.display += value.toString();
+        break;
+      case State.FirstFigure:
+        this.firstFigure = this.firstFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case State.SecondFigure:
+        this.secondFigure = this.secondFigure * 10 + value;
+        this.display += value.toString();
+        break;
+      case State.Result:
+        this.firstFigure = value;
+        this.secondFigure = 0;
+        this.result = 0;
+        this.operator = '';
+        this.currentState = State.FirstFigure;
+        this.display = value.toString();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  calculate(): number {
+    switch (this.operator) {
+      case '+':
+        return this.firstFigure + this.secondFigure;
+      case '-':
+        return this.firstFigure - this.secondFigure;
+      case '*':
+        return this.firstFigure * this.secondFigure;
+      case '/':
+        return this.firstFigure / this.secondFigure;
+      default:
+        return 0;
+    }
+  }
+
+  handleSymbol(value: string): void {
+    switch (this.currentState) {
+      case State.Init:
+
+        break;
+      case State.FirstFigure:
+        if(value === '+' || value === '-' || value === '*' || value === '/') {
+          this.operator = value;
+          this.currentState = State.SecondFigure;
+          this.display += value;
+        }
+        break;
+      case State.SecondFigure:
+        if(value === '='){
+          this.result = this.calculate();
+          this.currentState = State.Result;
+          this.display += value + this.result.toString();
+        }
+        break;
+      case State.Result:
+        if(value === '+' || value === '-' || value === '*' || value === '/') {
+          this.firstFigure = this.result;
+          this.operator = value;
+          this.secondFigure = 0;
+          this.result = 0;
+          this.currentState = State.SecondFigure;
+          this.display = this.firstFigure.toString() + value;
+        }
+        break;
+
+      default:
+        break;
+    }
+
+
+  }
+
+  handleClick(value: any) {
+    if (typeof value === 'number') {
+      this.handleNumber(value);
+
+    } else if (typeof value === 'string') {
+      this.handleSymbol(value);
+    }
+
   }
 
 }
